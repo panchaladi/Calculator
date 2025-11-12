25021833c93172fd0a9de06db60bdee100b7fc13
panchaladi <127315920+panchaladi@users.noreply.github.com>
2025-11-12 10:39:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result;
+        char operator;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        num1 = input.nextDouble();
+
+        System.out.println("Enter second number:");
+        num2 = input.nextDouble();
+
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) { // Prevent division by zero
+                    result = num1 / num2;
+                    System.out.println(num1 + " / " + num2 + " = " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        input.close();
+    }
+}
